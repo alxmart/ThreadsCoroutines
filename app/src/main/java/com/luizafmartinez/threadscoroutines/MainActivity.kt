5340626b4316d20fe95d443e306901044e36651e
alxmart <alxmartinez@gmail.com>
2023-12-19 20:55:43 -0300

    viewBinding and button btnAbrir configurations.

--- app/src/main/java/com/luizafmartinez/threadscoroutines/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/threadscoroutines/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.luizafmartinez.threadscoroutines
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luizafmartinez.threadscoroutines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnAbrir.setOnClickListener {
+            startActivity(
+                Intent(this, SegundaActivity::class.java)
+            )
+        }
+
+        binding.btnIniciar.setOnClickListener {
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
